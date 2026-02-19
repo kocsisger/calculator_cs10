74985a335c90569d37a7106b35d38ba259b92280
Kocsis Gergely <kocsisger@gmail.com>
2026-02-19 11:32:06 +0100

    Expression processed

--- app/src/main/java/hu/unideb/inf/calculator/MainActivity.java
+++ app/src/main/java/hu/unideb/inf/calculator/MainActivity.java
@@ -1,6 +1,7 @@
 package hu.unideb.inf.calculator;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -30,12 +31,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void handleButtonPressed(View view) {
-        Button button = (Button)view;
+        Button button = (Button) view;
 
-        switch (button.getText().toString()){
-            case "CE" : resultTextView.setText("0"); break;
-            case "="  : break;
-            default   : resultTextView.append(button.getText().toString());
+        switch (button.getText().toString()) {
+            case "CE":
+                resultTextView.setText("0");
+                break;
+            case "=": resultTextView.setText(
+                        calculate(resultTextView.getText().toString())
+                      ); break;
+            default:
+                resultTextView.append(button.getText().toString());
         }
+    }
+
+    private String calculate(String expression) {
+        String[] splitExpression = expression.split("[+\\-*/]");
+        int op1 = Integer.parseInt(splitExpression[0]);
+        int op2 = Integer.parseInt(splitExpression[1]);
+
+        String operator = expression.replace(splitExpression[0], "");
+        operator = operator.replace(splitExpression[1], "");
+
+        Log.d("TEXT_OP", "Operands:" + op1 + " " + operator + " " + op2);
+        return "ERROR";
     }
 }
